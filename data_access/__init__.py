ec41cee2899e5f24332297eb281d520de12f0a73
ThilinaGunasekara <thilina.dg93@gmail.com>
2025-10-03 03:19:08 +0530

    u

--- data_access/__init__.py
+++ data_access/__init__.py
@@ -1,18 +1,1 @@
-"""
-Data Access Layer
-Contains database operations, models, and data repositories
-"""
-
-from .models import (
-    Route, TrafficRecord, PredictionRequest, PredictionResult,
-    RouteComparison, WeatherData, RealTimeTrafficData, AnalyticsData,
-    DayType, WeatherCondition, TrafficDensity, SeasonType
-)
-from .database_manager import DatabaseManager, TrafficRecordRepository
-
-__all__ = [
-    'Route', 'TrafficRecord', 'PredictionRequest', 'PredictionResult',
-    'RouteComparison', 'WeatherData', 'RealTimeTrafficData', 'AnalyticsData',
-    'DayType', 'WeatherCondition', 'TrafficDensity', 'SeasonType',
-    'DatabaseManager', 'TrafficRecordRepository'
-]
+"""Data Access Layer"""
--- external_services/__init__.py
+++ external_services/__init__.py
@@ -1,15 +1,1 @@
-"""
-External Services Layer
-Contains API integrations and third-party service handlers
-"""
-
-from .api_handler import GoogleMapsAPIService, WeatherAPIService, DataCollectionService, APICoordinator
-from .weather_service import SriLankanWeatherService
-
-__all__ = [
-    'GoogleMapsAPIService',
-    'WeatherAPIService',
-    'DataCollectionService',
-    'APICoordinator',
-    'SriLankanWeatherService'
-]
+"""External Services"""
